364d97cd839197ce2d76b9377980472f3f7e296e
muhfikriw <fikdi1234@gmail.com>
2021-06-11 21:59:12 +0700

    website singo jowo

---
+++ login/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {             $('#loginModal').modal('show');
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+});
+
+$(document).ready(function() {             $('#loginModal1').modal1('show');
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+});
